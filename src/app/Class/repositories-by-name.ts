def881acf3811f0740f367eb166c61401d8cfea3
Joy Kirii <joykirii@gmail.com>
2021-04-05 20:10:39 +0300

    change logic of find repositories

---
+++ src/app/Class/repositories-by-name.ts
@@ -0,0 +1,9 @@
+export class RepositoriesByName {
+  constructor(
+        public name:string,
+        public description:string,
+        public language:string,
+        public html_url: string,
+        public forks:number,
+        public updated_at:Date ){}
+}
--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 // import { environment } from 'src/environments/environment';
 import { environment } from '../../environments/environment'
+import { RepositoriesByName } from '../Class/repositories-by-name'
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,7 @@ export class GithubService {
   apiKey:string;
   clientId: string = '761a10c6250fec1cd9c3';
   clientSecret: string = '57a0b5167d57892cfe5d6ab3a60bd0287cffeb61';
+  reposByName: RepositoriesByName[] = [];
 
   constructor(private http:HttpClient) { }
 
@@ -26,13 +29,42 @@ export class GithubService {
   updateUsername(username:string){
     this.username = username;
   }
-  searchRepos(){
-    return this.http.get('https://api.github.com/search/repositories?q=' + this.repoName, ({
-      headers: new HttpHeaders({Authorization: `token ${environment.apiKey}`})
-    }))
+  // searchRepos(){
+  //   return this.http.get('https://api.github.com/search/repositories?q=' + this.repoName, ({
+  //     headers: new HttpHeaders({Authorization: `token ${environment.apiKey}`})
+  //   }))
+  //   }
+  //   updateRepo(repo:string) {
+  //   this.repoName = repo;
+  // }
+  searchRepos(repoName){
+    interface repobyNameApiResponse{
+      items:[];
     }
-    updateRepo(repo:string) {
-    this.repoName = repo;
+    let promise= new Promise<void>((resolve, reject) => {
+      let arrayLength = this.reposByName.length;
+      for(let i=0; i< arrayLength; i++){
+        this.reposByName.pop()
+      }
+      this.http.get<repobyNameApiResponse>(`https://api.github.com/search/repositories?q=${repoName}`).toPromise().then(response =>{
+        for(let i = 0; i < response.items.length; i++){
+          let repoByName = new RepositoriesByName ("","","","",0,new Date());
+          repoByName.name =  response.items[i]["name"]
+          repoByName.description =  response.items[i]["description"]
+          repoByName.language =  response.items[i]["language"]
+          repoByName.html_url =  response.items[i]["html_url"]
+          repoByName.forks = response.items[i]["forks"]
+          repoByName.updated_at = response.items[i]["updated_at"]
+          this.reposByName.push(repoByName)
+          }
+          resolve()
+        },
+        error => {
+          console.log("error has occured")
+          reject(error)
+        })
+      })
+      return promise
   }
   }
   
